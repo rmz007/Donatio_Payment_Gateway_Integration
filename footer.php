653010abc3a7155dd1111a77bc28457283bef263
rmz007 <59342106+rmz007@users.noreply.github.com>
2020-10-07 22:24:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+	<title>Untitled Document</title>
+	
+	
+</head>
+<body>
+	<table width="1340" bgcolor="#999898" style=" color:white; bottom:0px; font-size:20px;">
+		<tr>
+		<td class = "f" align="center">The Sparks Foundation internship project by Rameez Rauf K</td>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Donation</title>
+
+<link rel ="stylesheet" href="css/style.css">
+
+</head>
+<body style="background-image: url('charity.jpg');background-repeat: no-repeat;background-size: cover;">
+
+	<div style="background-color:black;">
+		<img src = "donate3.gif"; height=120px; width=200px; border="1">
+		
+	</div>
+	
+	<div id="image">
+			<img src="logo2.png"; height=150px; width=190px; align=left>
+			<center><img src="logo.png""; height=150px; width=190px;>
+			<img src="logo3.png"; height=150px; width=190px; align=right>
+			<form><h1>CLICK HERE:</h1><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_Fm2edJMIL0Hvmi"> </script> </form>
+			</div>
+			<br><br><br>
+	<?php include 'footer.php'?>
+</body>
+</html>
